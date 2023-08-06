fe2c5bc09706d292a1e0f1c12c7b0c9c49085a8a
Simnaan <simnaan7860@hotmail.com>
2023-08-06 15:35:46 +0100

    Started the menu and declared the first fuction I will Create. I also created a Student class using the JSON file.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 ﻿
 using Newtonsoft.Json;
+using System.Linq.Expressions;
 
-    static void main()
+static void main()
 {
-    Console.WriteLine()
+    Console.WriteLine("Welcome to the University Management System");
+    Console.WriteLine("If you would like to view Student records, please type 1");
+    int Option = Convert.ToInt32(Console.ReadLine());
+
+    switch (Option)
+    {
+        case 1: ViewStudentRecord();
+            break;
+    }
 }
 
-   
+static void ViewStudentRecord()
+{
+
+}
+
+
+// var o = JsonConvert.DeserializeObject<Student>(System.IO.File.ReadAllText(@"c:\students.json"))
